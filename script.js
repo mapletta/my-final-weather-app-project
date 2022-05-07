4351f8e726acc3f5f3280489817d65e6f8076639
mapletta <anggaw0901@gmail.com>
2022-05-07 20:31:12 +0200

    API

---
+++ script.js
@@ -0,0 +1,7 @@
+function displayTemperature(response) {
+  console.log(response.data.main.temp);
+}
+let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Warsaw&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
