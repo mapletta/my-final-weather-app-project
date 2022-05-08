a29fcd97906fea9e5f9a3e0fbacdfe7baf999173
mapletta <anggaw0901@gmail.com>
2022-05-08 15:55:24 +0200

    adding real time date and time data

--- script.js
+++ script.js
@@ -1,3 +1,43 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let year = date.getFullYear();
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  let month = months[date.getMonth()];
+  let dayMonth = date.getDate();
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day}, ${dayMonth} ${month} ${year} ${hours}:${minutes}`;
+}
+
 function displayTemperature(response) {
   console.log(response.data);
   let temperatureElement = document.querySelector("#current-temperature");
@@ -5,11 +45,13 @@ function displayTemperature(response) {
   let descriptionElement = document.querySelector("#sky-description");
   let humidityElement = document.querySelector("#humidity-per");
   let windElement = document.querySelector("#wind-speed");
+  let dateElement = document.querySelector("#current-date");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
