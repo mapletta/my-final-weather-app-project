b470ae41d50304fbee8730552adc0193f6e33c04
mapletta <anggaw0901@gmail.com>
2022-05-17 17:33:14 +0200

    Update script.js

--- script.js
+++ script.js
@@ -104,7 +104,7 @@ function displayTemperature(response) {
 
   weatherIconElement.setAttribute(
     "src",
-    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+    `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
 
   getForecast(response.data.coord);
